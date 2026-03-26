52dfd00047b14a8a61c495b307ddaa257d16ac39
Hempreet Singh <hempreetsingh001@gmail.com>
2026-03-26 15:57:15 +0530

    Initial Commit

---
+++ src/test/java/com/example/emailsummarizer/EmailSummarizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emailsummarizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSummarizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
